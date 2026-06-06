45192bda3372feee63befd16a91b3368b2a4088b
CYLin <rogerline0527@gmail.com>
2026-06-06 19:06:46 +0800

    chore: Update playwright config, now it'll generate report into specified folder

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['list']],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: '../reports/frontend', open: 'never' }],
+  ],
   use: {
     baseURL: 'http://127.0.0.1:5173',
     trace: 'on-first-retry',
